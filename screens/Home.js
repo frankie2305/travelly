806919599cbe1803ff6a1d4d136659d44dbac064
frankie2305 <nguyenquoctrung115ltk@gmail.com>
2021-04-02 22:18:34 +0700

    Added categories to home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import { Screen, Text } from '../components';
+import { View } from 'react-native';
+import { Category, Screen, Text } from '../components';
 import { styles } from '../constants';
 
 export default Home = () => (
-	<Screen style={styles.center}>
-		<Text
-			color='blue'
-			style={{
-				fontFamily: 'roboto-black',
-				fontSize: 30,
-				marginBottom: 30,
-				textTransform: 'uppercase',
-			}}>
-			Home
-		</Text>
+	<Screen>
+		<View style={[styles.row, { justifyContent: 'space-around' }]}>
+			<Category
+				color='red'
+				icon='restaurant'
+				label='Restaurants'
+				onPress={() => console.log('Restaurants')}
+			/>
+			<Category
+				color='green'
+				icon='hotel'
+				label='Hotels'
+				onPress={() => console.log('Hotels')}
+			/>
+		</View>
+		<View style={[styles.row, { justifyContent: 'space-around' }]}>
+			<Category
+				color='blue'
+				icon='attractions'
+				label='Attractions'
+				onPress={() => console.log('Attractions')}
+			/>
+			<Category
+				color='yellow'
+				icon='local-activity'
+				label='Things to do'
+				onPress={() => console.log('Things to do')}
+			/>
+		</View>
 	</Screen>
 );
