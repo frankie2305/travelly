6be6226427e83b6297d786bc525b7cae448c0511
frankie2305 <nguyenquoctrung115ltk@gmail.com>
2021-04-05 18:57:40 +0700

    Rendered travels on travels screen

--- screens/Travels.js
+++ screens/Travels.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import { Screen, Text } from '../components';
+import React, { useContext } from 'react';
+import { View, FlatList } from 'react-native';
+import Constants from 'expo-constants';
+import { UserContext } from '../contexts';
+import { Card, Text } from '../components';
 import { styles } from '../constants';
 
-export default Travels = () => (
-	<Screen style={styles.center}>
-		<Text
-			color='blue'
+export default Travels = () => {
+	const { travels } = useContext(UserContext);
+
+	return (
+		<View
 			style={{
-				fontFamily: 'roboto-black',
-				fontSize: 30,
-				textTransform: 'uppercase',
+				flex: 1,
+				marginVertical: Constants.statusBarHeight,
+				marginHorizontal: 10,
 			}}>
-			Travels
-		</Text>
-	</Screen>
-);
+			<Text
+				color='blue'
+				style={[
+					styles.title,
+					{
+						textAlign: 'center',
+					},
+				]}>
+				My Travels
+			</Text>
+			<FlatList
+				data={travels}
+				keyExtractor={item => item.name}
+				renderItem={({ item, index }) => (
+					<Card color={index % 2 === 0 ? 'black' : 'white'}>
+						<View style={{ flex: 1 }}>
+							<Text
+								color={index % 2 === 0 ? 'white' : 'black'}
+								style={{ fontFamily: 'roboto-bold' }}>
+								{item.name}
+							</Text>
+							<Text
+								color={index % 2 === 0 ? 'white' : 'black'}
+								style={{ fontSize: 14 }}>
+								Destination:{' '}
+								{item.city[0].toUpperCase() +
+									item.city.substring(1)}
+							</Text>
+							<Text
+								color={index % 2 === 0 ? 'white' : 'black'}
+								style={{ fontSize: 12 }}>
+								{item.restaurants.length} restaurant
+								{item.restaurants.length !== 1 && 's'},{' '}
+								{item.hotels.length} hotel
+								{item.hotels.length !== 1 && 's'},{' '}
+								{item.attractions.length} attraction
+								{item.attractions.length !== 1 && 's'},{' '}
+								{item.activities.length} thing
+								{item.activities.length !== 1 && 's'} to do
+							</Text>
+						</View>
+					</Card>
+				)}
+			/>
+		</View>
+	);
+};
