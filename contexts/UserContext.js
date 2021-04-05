81d79ef8530693024b9375102b4c028be5c5f657
frankie2305 <nguyenquoctrung115ltk@gmail.com>
2021-04-05 18:52:05 +0700

    Added travels to user context

--- contexts/UserContext.js
+++ contexts/UserContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useCallback, useState } from 'react';
+import { db } from '../constants';
 
 export const UserContext = createContext();
 
@@ -9,8 +10,32 @@ export const UserContextProvider = ({ children }) => {
 		user,
 	]);
 
+	const [travels, setTravelsState] = useState([
+		{
+			name: 'Sydney Test',
+			city: 'sydney',
+			restaurants: [db.sydney.restaurants[0]],
+			hotels: [db.sydney.hotels[0]],
+			attractions: [db.sydney.attractions[0]],
+			activities: [db.sydney.activities[0]],
+		},
+		{
+			name: 'Melbourne Test',
+			city: 'melbourne',
+			restaurants: [db.melbourne.restaurants[0]],
+			hotels: [db.melbourne.hotels[0]],
+			attractions: [db.melbourne.attractions[0]],
+			activities: [db.melbourne.activities[0]],
+		},
+	]);
+
+	const setTravels = useCallback(
+		currentTravels => setTravelsState(currentTravels),
+		[travels]
+	);
+
 	return (
-		<UserContext.Provider value={{ user, setUser }}>
+		<UserContext.Provider value={{ user, setUser, travels, setTravels }}>
 			{children}
 		</UserContext.Provider>
 	);
