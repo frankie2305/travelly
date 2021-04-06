e149afb0e80dfb64f60d75146ba8831188424986
frankie2305 <nguyenquoctrung115ltk@gmail.com>
2021-04-06 10:31:15 +0700

    Added more travels to user context

--- contexts/UserContext.js
+++ contexts/UserContext.js
@@ -1,5 +1,4 @@
 import React, { createContext, useCallback, useState } from 'react';
-import { db } from '../constants';
 
 export const UserContext = createContext();
 
@@ -12,20 +11,105 @@ export const UserContextProvider = ({ children }) => {
 
 	const [travels, setTravelsState] = useState([
 		{
-			name: 'Sydney Test',
+			name: 'Sydney Test 0',
 			city: 'sydney',
-			restaurants: [db.sydney.restaurants[0]],
-			hotels: [db.sydney.hotels[0]],
-			attractions: [db.sydney.attractions[0]],
-			activities: [db.sydney.activities[0]],
+			restaurants: [],
+			hotels: [],
+			attractions: [],
+			activities: [],
 		},
 		{
-			name: 'Melbourne Test',
+			name: 'Melbourne Test 0',
 			city: 'melbourne',
-			restaurants: [db.melbourne.restaurants[0]],
-			hotels: [db.melbourne.hotels[0]],
-			attractions: [db.melbourne.attractions[0]],
-			activities: [db.melbourne.activities[0]],
+			restaurants: [],
+			hotels: [],
+			attractions: [],
+			activities: [],
+		},
+		{
+			name: 'Sydney Test 1',
+			city: 'sydney',
+			restaurants: ['Cucina Porto'],
+			hotels: ['The Fullerton Hotel Sydney'],
+			attractions: ['Sydney Opera House'],
+			activities: ['Chinese Garden General Admission Ticket'],
+		},
+		{
+			name: 'Melbourne Test 1',
+			city: 'melbourne',
+			restaurants: ['Foglia di Fico'],
+			hotels: ['Park Hyatt Melbourne'],
+			attractions: ['Royal Botanic Gardens Victoria'],
+			activities: ['Yarra Valley Wine and Winery Tour from Melbourne'],
+		},
+		{
+			name: 'Sydney Test 2',
+			city: 'sydney',
+			restaurants: ['Cucina Porto', 'Farmhouse Kings Cross'],
+			hotels: ['The Fullerton Hotel Sydney', 'QT Sydney'],
+			attractions: ['Sydney Opera House', 'Sydney Harbour Bridge'],
+			activities: [
+				'Chinese Garden General Admission Ticket',
+				'Sydney The Rocks Guided Walking Tour',
+			],
+		},
+		{
+			name: 'Melbourne Test 2',
+			city: 'melbourne',
+			restaurants: ['Foglia di Fico', 'Scopri'],
+			hotels: ['Park Hyatt Melbourne', 'QT Melbourne'],
+			attractions: [
+				'Royal Botanic Gardens Victoria',
+				'Melbourne Cricket Ground (MCG)',
+			],
+			activities: [
+				'Yarra Valley Wine and Winery Tour from Melbourne',
+				'SEA LIFE Melbourne Aquarium Admission Ticket',
+			],
+		},
+		{
+			name: 'Sydney Test 3',
+			city: 'sydney',
+			restaurants: [
+				'Cucina Porto',
+				'Farmhouse Kings Cross',
+				'Thai Pothong',
+			],
+			hotels: [
+				'The Fullerton Hotel Sydney',
+				'QT Sydney',
+				'Sir Stamford at Circular Quay Hotel Sydney',
+			],
+			attractions: [
+				'Sydney Opera House',
+				'Sydney Harbour Bridge',
+				'Royal Botanic Garden Sydney',
+			],
+			activities: [
+				'Chinese Garden General Admission Ticket',
+				'Sydney The Rocks Guided Walking Tour',
+				'All Inclusive Blue Mountains Small-Group Day Trip from Sydney',
+			],
+		},
+		{
+			name: 'Melbourne Test 3',
+			city: 'melbourne',
+			restaurants: ['Foglia di Fico', 'Scopri', 'Da Guido La Pasta'],
+			hotels: [
+				'Park Hyatt Melbourne',
+				'QT Melbourne',
+				'Crown Towers Melbourne',
+			],
+			attractions: [
+				'Royal Botanic Gardens Victoria',
+				'Melbourne Cricket Ground (MCG)',
+				'National Gallery of Victoria',
+			],
+			activities: [
+				'Yarra Valley Wine and Winery Tour from Melbourne',
+				'SEA LIFE Melbourne Aquarium Admission Ticket',
+				'Spirit of Melbourne Dinner Cruise',
+			],
 		},
 	]);
 
