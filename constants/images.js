3b5da667956acf4f5dc5adab32012e65fcfd9dcb
frankie2305 <nguyenquoctrung115ltk@gmail.com>
2021-04-07 13:45:05 +0700

    Added images to constants

---
+++ constants/images.js
@@ -0,0 +1,8 @@
+export default images = {
+	welcome: require('../assets/welcome.png'),
+	home: require('../assets/home.png'),
+	// By Benh LIEU SONG (Flickr) - Sydney&#039;s Landmarks 2, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=82284289
+	sydney: require('../assets/sydney.jpg'),
+	// By Montage by HappyWaldo - Own work by uploader created from licence-free images from Commons., CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=78098825
+	melbourne: require('../assets/melbourne.jpg'),
+};
--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,6 @@
 import colors from './colors';
 import db from './db';
+import images from './images';
 import styles from './styles';
 
-export { colors, db, styles };
+export { colors, db, images, styles };
